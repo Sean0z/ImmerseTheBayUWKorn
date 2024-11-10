83a243804454915d79dd4328b51709011d6ee350
Sean0z <seanozinsky@gmail.com>
2024-11-10 03:49:18 -0800

    Initial Commit

---
+++ Assets/Scripts/SceneReset.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class SceneReset : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
